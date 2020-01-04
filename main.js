236c335867eb5620ae100dbbe6b7409fa7ac1909
Pablo-Roncero <pablo.roncero.flores@gmail.com>
2020-01-04 19:51:55 +0100

    Archivo js con resta, multiplicacion y division incluidas, html correccion de errores.

--- main.js
+++ main.js
@@ -18,21 +18,72 @@ function input(elem) {
 
 function getResult() {
     var res = document.getElementById("resultado");
-    if (res.innerHTML.charAt(res.innerHTML.length - 1) === "+" || resultado.innerHTML === "" || resultado.innerHTML.includes("=")) {
-        //Do nothing
-    } else {
-        var numbers = res.innerHTML.split("+");
-        var sum = 0;
-
-        for (var i = 0; i < numbers.length; i++) {
-            numbers[i] = Number(numbers[i]);
-            sum += numbers[i];
+    if (res.innerHTML.includes("+")) {
+        if (res.innerHTML.charAt(res.innerHTML.length - 1) === "+" || resultado.innerHTML === "" || resultado.innerHTML.includes("=")) {
+            //Do nothing
+        } else {
+            var numbers = res.innerHTML.split("+");
+            var sum = 0;
+    
+            for (var i = 0; i < numbers.length; i++) {
+                numbers[i] = Number(numbers[i]);
+                sum += numbers[i];
+            }
+    
+            res.innerHTML = res.innerHTML + " = " + sum;
         }
 
-        res.innerHTML = res.innerHTML + " = " + sum;
+    } else if (res.innerHTML.includes("-")) {
+        if (res.innerHTML.charAt(res.innerHTML.length - 1) === "-" || resultado.innerHTML === "" || resultado.innerHTML.includes("=")) {
+            //Do nothing
+        } else {
+            var numbers = res.innerHTML.split("-");
+            var resta;
+    
+            for (var i = 0; i < numbers.length; i++) {
+                numbers[i] = Number(numbers[i]);
+                resta = numbers[0];
+                resta -= numbers[i];
+            }
+    
+            res.innerHTML = res.innerHTML + " = " + resta;
+        }
+
+    } else if (res.innerHTML.includes("*")) {
+        if (res.innerHTML.charAt(res.innerHTML.length - 1) === "*" || resultado.innerHTML === "" || resultado.innerHTML.includes("=")) {
+            //Do nothing
+        } else {
+            var numbers = res.innerHTML.split("*");
+            var mult;
+    
+            for (var i = 0; i < numbers.length; i++) {
+                numbers[i] = Number(numbers[i]);
+                mult = numbers[0];
+                mult *= numbers[i];
+            }
+    
+            res.innerHTML = res.innerHTML + " = " + mult;
+        }
+
+    } else if (res.innerHTML.includes("/")) {
+        if (res.innerHTML.charAt(res.innerHTML.length - 1) === "/" || resultado.innerHTML === "" || resultado.innerHTML.includes("=")) {
+            //Do nothing
+        } else {
+            var numbers = res.innerHTML.split("/");
+            var div;
+    
+            for (var i = 0; i < numbers.length; i++) {
+                numbers[i] = Number(numbers[i]);
+                div = numbers[0];
+                div /= numbers[i];
+            }
+    
+            res.innerHTML = res.innerHTML + " = " + div;
+        }
 
     }
 }
+
 
 function clearResult() {
     document.getElementById("resultado").innerHTML = "";
